0b56bea36c30287f33b71fb1b7f9664a2a393680
NotUser1 <mikolaj@jaworscy.name>
2025-05-23 17:54:33 +0200

    New webpage design

--- cybersecurity/src/App.tsx
+++ cybersecurity/src/App.tsx
@@ -1,35 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="logo">
+        <p>Cyberbezpieczeństwo</p>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+      <h1>
+        Temat 2 - Instalacja kupionego albo wygenerowanego certyfikatu dla
+        serwera WWW
+      </h1>
+      <p>
+        Jakub Dąbrowski 197629 <br />
+        Łukasz Rohde 197672 <br />
+        Mikołaj Jaworski 197636
       </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
